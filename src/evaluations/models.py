c35e5e5a06021862fda4218af0090cce3356c5ca
Gedoix <diegoortegop@hotmail.com>
2019-05-11 18:24:45 -0400

    Added evaluations model

--- src/evaluations/models.py
+++ src/evaluations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Evaluation(models.Model):
+
+    rubric = models.ForeignKey("rubrics.Rubric", null=True, on_delete=models.SET_NULL)
+    course = models.OneToOneField("courses.Course", primary_key=True, on_delete=models.CASCADE)
