3e5f9c710349d7b24145dbec43d1254bdece543e
BryanRrs <bryanrrs@gmail.com>
2019-05-20 16:44:32 -0400

    probando

--- src/auto-migrate.sh
+++ src/auto-migrate.sh
@@ -4,5 +4,11 @@ source ../venv/bin/activate
 
 python3 manage.py makemigrations users
 python3 manage.py migrate
-python3 manage.py makemigrations
+python3 manage.py makemigrations courses
+python3 manage.py migrate
+python3 manage.py makemigrations presentations
+python3 manage.py migrate
+python3 manage.py makemigrations rubrics
+python3 manage.py migrate
+python3 manage.py makemigrations evaluations
 python3 manage.py migrate
