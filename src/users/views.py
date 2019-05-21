4fa22be50d4ba0ed3d75964c3fba139ccd3c3cad
BryanRrs <bryanrrs@gmail.com>
2019-05-20 22:06:47 -0400

    views y template landing

--- src/users/views.py
+++ src/users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from rubrics.models import Rubric
 # Create your views here.
 
 def landing_page(request, *args):
-
-    return render(request, 'landing_page.html')
+    queryset = Rubric.objects.all()[::-1]
+    context = {
+        "my_rubrics" : queryset
+    }
+    return render(request, 'landing_page.html',context)
