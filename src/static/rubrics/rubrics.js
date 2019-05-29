270a80c6115cdf60aa08644e3eae637837f3647e
Gedoix <diegoortegop@hotmail.com>
2019-05-29 01:26:07 -0400

    Finished rubrics view implementation

--- src/static/rubrics/rubrics.js
+++ src/static/rubrics/rubrics.js
@@ -2,13 +2,25 @@
 
 function detailToggle( id )
 {
-    $( `#${id}` ).toggleClass("w3-show");
+    $( `#${id}` )
+        .toggleClass( "w3-show" );
+}
+
+function deleteDialogToggle( dialog_id )
+{
+    $( `#${dialog_id}` )
+        .css( "display", ( index, current_value ) =>
+        {
+            return current_value === "none" ? "block" : "none";
+        } );
 }
 
 $(
-    function()
+    function ()
     {
-        $( ".rubric_table" ).addClass("w3-table-all");
-        $( ".rubric_table thead" ).addClass("w3-light-grey");
+        $( ".rubric_table" )
+            .addClass( "w3-table-all" );
+        $( ".rubric_table thead" )
+            .addClass( "w3-light-grey" );
     }
 )
