888a673f105d235ede852daf20fdc699c9b0e132
Gedoix <diegoortegop@hotmail.com>
2019-05-30 18:19:54 -0400

    Fixed auto-migrate

--- src/auto-migrate.sh
+++ src/auto-migrate.sh
@@ -6,6 +6,8 @@ python3 manage.py makemigrations users
 python3 manage.py migrate
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py makemigrations rubrics
+python3 manage.py makemigrations courses
 python3 manage.py makemigrations evaluations
+python3 manage.py makemigrations presentations
+python3 manage.py makemigrations rubrics
 python3 manage.py migrate
