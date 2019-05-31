7a18be4312d33aafe17445192ce220384bc0d318
Valeria <valeriaelisav@gmail.com>
2019-05-31 19:37:36 -0400

    Evaluaciones para evaluadores, muestra más de 10 y no redirige

--- src/evaluations/views.py
+++ src/evaluations/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from presentations.models import *
+from .models import Evaluation
+from django.db.models.query import QuerySet
+
 # Create your views here.
 
 
 def evaluations_page(request, *args):
-    return render(request, "evaluations.html")
+    if request.user.is_staff:
+        return render(request, "evaluations.html")
+    else:
+        userid = request.user.id
+        grades = Grade.objects.filter(evaluator_id = userid)[:10] #All grades for the evaluator
+        context = {
+            'obj' : grades
+        }
+        return render(request, "evaluations.html", context)
