900310de2f913d143b37ed3bdec785ba43b149ee
BryanRrs <bryanrrs@gmail.com>
2019-06-01 00:43:45 -0400

    Se pueden crear evaluadores

--- src/project/urls.py
+++ src/project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 
-from users.views import landing_page, evaluators_page
+from users.views import landing_page, evaluators_page, registro_usuario
 from courses.views import courses_page
 from evaluations.views import evaluations_page
 from rubrics.views import rubrics_page
@@ -25,7 +25,8 @@ from rubrics.views import rubrics_page
 urlpatterns = [
     path('', landing_page, name="Landing Page"),
     path('cursos/', courses_page, name="Courses Page"),
-    path('evaluadores/', evaluators_page, name="Evaluators Page"),
+    #path('evaluadores/', evaluators_page, name="Evaluators Page"),
+    path('evaluadores/', registro_usuario.as_view(), name="Evaluators Page"),
     path('rubricas/', rubrics_page, name="Rubrics Page"),
     path('rubricas/', include('rubrics.urls'), name="Rubrics Editor"),
     path('evaluaciones/', evaluations_page, name="Evaluations Page"),
--- src/users/views.py
+++ src/users/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 # Create your views here.
+from .forms import BaseUserCreationForm
+from django.views.generic import CreateView
+from .models import BaseUser
+from django.contrib import messages
 
 def landing_page(request, *args):
 
     return render(request, 'landing_page.html')
 
 def evaluators_page(request, *args):
+    form = BaseUserCreationForm
+    return render(request, 'evaluators.html', {'form':form})
 
-    return render(request, 'evaluators.html')
+class registro_usuario(CreateView):
+    model = BaseUser 
+    template_name = 'evaluators.html'
+    form_class = BaseUserCreationForm
+
+    def get_context_data(self, **kwargs):
+        ctx = super(registro_usuario,self).get_context_data(**kwargs)
+        ctx['evaluators'] = BaseUser.objects.filter(is_staff=False)
+        return ctx
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, form['username'].value() +"  "+form['password1'].value())
+        return render(self.request, 'evaluators.html', self.get_context_data())
