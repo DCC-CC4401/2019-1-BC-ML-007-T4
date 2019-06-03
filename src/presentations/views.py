1df7e9039aaa2c5839f37823735c409c36a0c692
Gedoix <diegoortegop@hotmail.com>
2019-06-02 20:41:32 -0400

    Implemented evaluators list

--- src/presentations/views.py
+++ src/presentations/views.py
@@ -6,17 +6,46 @@ from .models import Presentation, Grade
 
 def evaluation_form_page(request, evaluation_id, group_id, *args):
 
-    presentation = get_object_or_404(Presentation, evaluation_id=evaluation_id, group_id=group_id)
+    presentation: Presentation = get_object_or_404(Presentation, evaluation_id=evaluation_id, group_id=group_id)
 
-    evaluation = presentation.evaluation
+    presentators = presentation.presentators
 
-    allowed_evaluators = presentation.allowed_evaluators.all()
+    evaluation: Evaluation = presentation.evaluation
+
+    allowed_evaluators_set = presentation.allowed_evaluators.all()
+
+    allowed_evaluators = []
+
+    group_members = presentation.group.student_set.all()
+
+    group_member_statuses = []
 
     grades = presentation.grade_set.all()
 
+    answered = grades.filter(state=True, evaluator=request.user).count() == grades.filter(evaluator=request.user).count()
+
+    for evaluator in allowed_evaluators_set:
+
+        evaluator_answered_grades = grades.filter(state=True, evaluator=evaluator, presentation=presentation).count()
+
+        evaluator_total_grades = grades.filter(evaluator=evaluator, presentation=presentation).count()
+
+        evaluator_expected_grades = presentators.all().count()
+
+        if evaluator_total_grades != evaluator_expected_grades:
+            evaluator_status = "pending"
+        elif evaluator_answered_grades != evaluator_expected_grades:
+            evaluator_status = "evaluating"
+        else:
+            evaluator_status = "done"
+
+        allowed_evaluators.append((evaluator.username, evaluator_status))
+
     context = {
-        "evaluation": presentation,
-        "answered": grades,
+        "allowed_evaluators": allowed_evaluators,
+        "group_members": group_members,
+        "presentation": presentation,
+        "answered": answered,
     };
 
     return render(request, "evaluation_form.html", context);
