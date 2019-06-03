da38d2b37aeb0ae9fde0ba8246ddcddc844a7414
BryanRrs <bryanrrs@gmail.com>
2019-06-03 15:32:45 -0400

    Visualización de cursos

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 
 def courses_page(request, *args):
-
-    return render(request, 'courses.html')
+    if request.user.is_staff==True:
+        queryset = Course.objects.filter(owner = request.user)
+        return render(request, 'courses.html', {'Courses':queryset})
+    else:
+        return render(request, 'landing_page.html')
