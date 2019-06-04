fa757a10ae8669533d77a03305d8e6873005f92a
Miguel-SH <masepulh@gmail.com>
2019-06-03 23:47:24 -0400

    Se guardan las notas cuando se envia una evaluación.

--- src/presentations/urls.py
+++ src/presentations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import evaluation_form_page, update_context_view
+from .views import evaluation_form_page, update_context_view, upload_grade
 
 app_name = 'presentations'
 urlpatterns = [
     path('evaluar/<int:evaluation_id>/<int:group_id>', evaluation_form_page, name='form'),
+    path('evaluar/<int:evaluation_id>/<int:group_id>/subir_nota', upload_grade, name='upload_grade'),
     path('evaluar/<int:evaluation_id>/<int:group_id>/update_context', update_context_view, name='ajax_update'),
 ]
